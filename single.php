e9ab7670ffdb5ad3247e03718ffe33ddadbd7af4
ridz <ridz@shadow.com.bd>
2026-02-25 10:14:02 +0000

    25

--- single.php
+++ single.php
@@ -929,12 +929,8 @@ echo strtr(get_the_date('d F Y').' | '.get_the_time('H:i'), ['0'=>'০','1'=>'�
                                 <li>বিষয় :</li>
                                 <?php
                                 $post_tags = get_the_tags();
-                                if ( $post_tags ) {
-                                    echo '<ul class="post-tags">';
-                                    foreach ( $post_tags as $tag ) {
-                                        echo '<li><a href="'.esc_url(get_tag_link($tag->term_id)).'">'.esc_html($tag->name).'</a></li>';
-                                    }
-                                    echo '</ul>';
+                                foreach ( $post_tags as $tag ) {
+                                    echo '<li><a href="'.esc_url(get_tag_link($tag->term_id)).'">'.esc_html($tag->name).'</a></li>';
                                 }
                                 ?>
                             </ul>
