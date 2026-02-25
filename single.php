bca9a90b15866e0370db8478f5fecfd24ab657f0
ridz <ridz@shadow.com.bd>
2026-02-25 09:13:05 +0000

    8

--- single.php
+++ single.php
@@ -468,7 +468,7 @@
                     </div>
                     <div class="col-lg-4 col-12 d-flex justify-content-center align-items-center">
                         <div class="DLogo">
-                            <a href="<?php echo esc_url( home_url('/') ); ?>" class="DLogo" rel="home"><img src="<?php echo esc_url( if ( function_exists( 'the_custom_logo' ) && has_custom_logo() ) { the_custom_logo(); } else { get_template_directory_uri() . '/images/logo.png'; } ); ?>" title="<?php the_title(); ?>" alt="<?php the_title(); ?>" class="img-fluid img100"></a>
+                            <a href="<?php echo esc_url( home_url('/') ); ?>" class="DLogo" rel="home"><img src="<?php echo esc_url( ( function_exists('get_custom_logo') && has_custom_logo() ) ? wp_get_attachment_image_url( get_theme_mod('custom_logo'), 'full' ) : get_template_directory_uri() . '/images/logo.png' ); ?>" title="<?php the_title(); ?>" alt="<?php the_title(); ?>" class="img-fluid img100"></a>
                         </div>
                     </div>
                     <div class="col-lg-4 col-sm-12 d-flex justify-content-end align-items-center">
